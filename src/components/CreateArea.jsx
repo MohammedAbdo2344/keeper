f1904c6c534e99279f93e506beb3fc11e5f8dd65
mohammed hassan <mh2900001@gmail.com>
2023-05-01 06:38:08 +0300

    Completed

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,51 +1,66 @@
 import React, { useState } from "react";
-
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import { Zoom } from "@mui/material";
+import { ZoomIn } from "@mui/icons-material";
 function CreateArea(props) {
-  const [note, setNote] = useState({
-    title: "",
-    content: ""
-  });
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-
-    setNote(prevNote => {
-      return {
-        ...prevNote,
-        [name]: value
-      };
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
     });
-  }
 
-  function submitNote(event) {
-    props.onAdd(note);
-    setNote({
-      title: "",
-      content: ""
-    });
-    event.preventDefault();
-  }
+    const [isExpended, setExpended] = useState(false);
 
-  return (
-    <div>
-      <form>
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
-        <textarea
-          name="content"
-          onChange={handleChange}
-          value={note.content}
-          placeholder="Take a note..."
-          rows="3"
-        />
-        <button onClick={submitNote}>Add</button>
-      </form>
-    </div>
-  );
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            };
+        });
+    }
+
+    function submitNote(event) {
+        props.onAdd(note);
+        setNote({
+            title: "",
+            content: ""
+        });
+        event.preventDefault();
+    }
+    function zoomOut() {
+        setExpended(true)
+    }
+
+    return (
+        <div>
+            <form className="create-note">
+                {
+                    isExpended ? <input
+                        name="title"
+                        onChange={handleChange}
+                        value={note.title}
+                        placeholder="Title"
+                    /> : null
+                }
+
+                <textarea
+                    name="content"
+                    onChange={handleChange}
+                    onClick={zoomOut}
+                    value={note.content}
+                    placeholder="Take a note..."
+                    rows={isExpended ? "3" : "1"}
+                />
+                <Zoom in={isExpended}>
+                    <Fab onClick={submitNote}><AddIcon /></Fab>
+                </Zoom>
+
+            </form>
+        </div>
+    );
 }
 
 export default CreateArea;
